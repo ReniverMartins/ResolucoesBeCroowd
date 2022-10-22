7ba9fa951ee3fb7f0908c43d9bdbb62820a7fbb0
Reniver Martins <r.kennyster@icloud.com>
2022-10-22 13:11:13 -0300

    Resolucao

---
+++ src/Cod1000.java
@@ -0,0 +1,7 @@
+public class Cod1000{
+    //HelloWorld
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");        
+    }
+}
---
+++ src/Cod1002.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Cod1002 {    
+/* Fórmula para calcular a área de uma circunferência: raio = π.(raio.raio)
+Considerar π = 3.14159:
+
+A entrada contém um valor de ponto flutuante (dupla precisão), no caso, a variável raio.*/
+    
+    public static void main(String[] args) {
+    Scanner input = new Scanner (System.in);
+
+        double raio, area;
+    
+            raio = input.nextDouble();
+            area = 3.14159 * Math.pow(raio,2);
+
+        System.out.printf("Area = "+"%.4f",area);
+
+    }
+
+
+}
